114bbe89d1ae6337e4561be9751b33156b9f72be
hussein-elmlah <hussein.elmlah7@gmail.com>
2024-02-17 07:05:42 +0200

    intial commit

---
+++ Lab5 - v1/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+const todoRoutes = require("./routes/todoRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/todosDB";
+
+// Connect to MongoDB database
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB database");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err.message);
+  });
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Define route handling middleware for specific paths.
+app.use("/users", userRoutes);
+app.use("/todos", todoRoutes);
+
+// Route handling middleware for handling all unmatched routes with a 404 response.
+app.use("*", (req, res) => {
+  res.status(404).send("Not found");
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status).send(`Something broke! \n ${err.message}`);
+});
+
+// Error handling for uncaught exceptions
+process.on("uncaughtException", function (err) {
+  console.log("Uncaught exception occurred:", err);
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
