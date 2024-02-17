9473a5759a4cb5604d67b40f2eec017cb44707c2
hussein-elmlah <hussein.elmlah7@gmail.com>
2024-02-17 07:07:29 +0200

    Fix: Route handling middleware for handling all unmatched routes

--- Lab4 - v5/index.js
+++ Lab4 - v5/index.js
@@ -6,8 +6,7 @@ const todoRoutes = require("./routes/todoRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/todosDB";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/todosDB";
 
 // Connect to MongoDB database
 mongoose
@@ -19,19 +18,24 @@ mongoose
     console.error("Error connecting to MongoDB:", err.message);
   });
 
-// Middleware
+// Middlewares
 app.use(express.json());
 app.use(cors());
 
-// Routes
+// Define route handling middleware for specific paths.
 app.use("/users", userRoutes);
 app.use("/todos", todoRoutes);
 
+// Route handling middleware for handling all unmatched routes with a 404 response.
+app.use("*", (req, res) => {
+  res.status(404).send("Not found");
+});
+
 // Error handling middleware
 app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(err.status).send(`Something broke! \n ${err.message}`)
-})
+  console.error(err.stack);
+  res.status(err.status).send(`Something broke! \n ${err.message}`);
+});
 
 // Error handling for uncaught exceptions
 process.on("uncaughtException", function (err) {
